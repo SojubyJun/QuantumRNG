dc17a03580611912ba5d9ff68b592b3b5bbe507c
Deshmugdha <mugdha.deshmukh18@gmail.com>
2026-07-27 12:28:32 -0700

    Update New Python Script.py

--- New Python Script.py
+++ New Python Script.py
@@ -1,11 +1,12 @@
-import cirq
+
 import random
 import secrets
 from math import gcd
 
 #prime checker (miller-rabin?)
 
-num_bits = 4
+num_bits = 8
+
 #random number generator functions
 def make_classical_bit(num_bits):
     return random.getrandbits(num_bits)
@@ -16,6 +17,10 @@ def make_qrng_bit(num_bits):
 def make_csprng_bit(num_bits):
     return secrets.randbits(num_bits)
 
+def make_trng_bit(num_bits):
+    pass
+
+#rng_choice = 
 def generate_prime(rng_choice):   #(rng choice):
     pass
 
