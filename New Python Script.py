09b734bdf4b0d0c839ae9a6d7cea586dc5bbeaf4
Deshmugdha <mugdha.deshmukh18@gmail.com>
2026-07-28 10:28:15 -0700

    Update New Python Script.py

--- New Python Script.py
+++ New Python Script.py
@@ -1,9 +1,17 @@
 
 import random
+import cirq
 import secrets
 from math import gcd
 
-#prime checker (miller-rabin?)
+#prime checker 
+def is_prime(n):
+    if n <2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
 
 num_bits = 8
 
@@ -18,6 +26,7 @@ def make_csprng_bit(num_bits):
     return secrets.randbits(num_bits)
 
 def make_trng_bit(num_bits):
+    #get from random.org
     pass
 
 #rng_choice = 
@@ -25,4 +34,41 @@ def generate_prime(rng_choice):   #(rng choice):
     pass
 
 #Convert to integer
+
+#generate prime:
+def generate_prime(rng_choice):
+    while True:
+        if rng_choice == 'PRNG':
+            candidate = make_classical_bit(num_bits)
+        elif rng_choice == 'CSPRNG':
+            candidate = make_csprng_bit(num_bits)
+        elif rng_choice == 'TRNG':
+            candidate = make_trng_bit(num_bits)
+        elif rng_choice == 'QRNG': 
+            candidate = make_qrng_bit(num_bits)
+
+        candidate |= 1
+        if is_prime(candidate):
+            return candidate
+
+# RSA Key Generation
+
+my_choice = input("Choose a random number generator (PRNG, CSPRNG, TRNG, QRNG): ")
+p = generate_prime(my_choice)
+q = generate_prime(my_choice)
+while p == q:
+    q = generate_prime(my_choice)
+
+n = p * q
+phi = (p - 1) * (q - 1)
+e = 65537 
+if gcd(e, phi) != 1:
+    e = 3
+d = pow(e, -1, phi)
+
+
+
+
+
+
   
